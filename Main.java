9d1dd5462a8b3fd6e1395962e070b5742c4fd3b6
Banogrono <48155039+Banogrono@users.noreply.github.com>
2020-08-24 05:12:11 +0200

    Final version
    Meets the requirements of https://github.com/florinpop17/app-ideas/blob/master/Projects/1-Beginner/Bin2Dec-App.md

--- Main.java
+++ Main.java
@@ -1,22 +1,50 @@
+/*
+    Binary to Decimal converter.
+
+    Simple programing exercise. Last edit 24/08/2020 | 05:09 | by s.
+
+    -----------------------------------------------------------------
+ */
+
+
 import java.util.Scanner;
 
 public class Main {
+
+    //  converts binary to decimal (might want to replace var length with binaryNumber.length())
+    static int bin2dec (String binaryNumber) {
+        int length = binaryNumber.length();
+        int decimalNumber = 0;
+
+        for (int i = 0; i < length; i++) {
+            decimalNumber += Character.getNumericValue(binaryNumber.charAt(i)) * Math.pow(2, length -1 -i);
+        }
+        return decimalNumber;
+    }
 
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
+        boolean isBinaryCorrect = true;
+
         System.out.print("Please provide a binary number for conversion: ");
         String binaryNumber = scanner.nextLine();
-        int decimalNumber = 0;
 
-        int power = 0;
-        for (int i = (binaryNumber.length() -1); i > 0; i--) {
-            decimalNumber += Character.getNumericValue(binaryNumber.charAt(i)) * (int) Math.pow(2,power);
-            power++;
+        // check if entered digits are binary and set flag to false if they are not
+        for (int i = 0; i < binaryNumber.length(); i++) {
+            if (binaryNumber.charAt(i) != '1' && binaryNumber.charAt(i) != '0') {
+                isBinaryCorrect = false;
+                break;
+            }
         }
-        
-        System.out.println(decimalNumber);
 
+        // check flag
+        if (!isBinaryCorrect) {
+            System.out.println("You can enter only binary digits (0 and 1).");
+            return;
+        }
+
+        System.out.println( "The binary " + binaryNumber + " is equal to " + bin2dec(binaryNumber) + " decimal.");
     }
 }
